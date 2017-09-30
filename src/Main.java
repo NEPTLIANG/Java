7c6e0db474d116ad4e2cc334446feace504c2ee0
NEPTLIANG <MING19980628@OUTLOOK.COM>
2017-09-30 05:18:23 +0000

    Java练习

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("HA");
+		
+		//MING到此一游
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("请问");
+		double MING = input.nextDouble();
+		
+		if (MING == 1.0);
+		{
+			LUO();
+		}
+	}
+	
+	public static void LUO()
+	{
+		for (int M = 6950; M < 8890; M--);
+		{
+			System.out.println("HA");
+		}
+	}
+}
