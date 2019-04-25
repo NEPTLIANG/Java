bc9b2e46540a872b8163174d80c02648758063d0
NeptLiang <neptliang@outlook.com>
2019-04-25 16:02:09 +0800

    Daily backup

--- Experiment4/src/Experiment4.java
+++ Experiment4/src/Experiment4.java
@@ -7,13 +7,15 @@ public class Experiment4 {
 
 class TestMatrix {
     public static Scanner reader = new Scanner(System.in);
+    //输入行数、列数
     public static int arows = reader.nextInt();
     public static int acols = reader.nextInt();
+    //声明数组
     public static int[][] a = new int[arows][acols];
     public static int[][] b = new int[acols][arows];
 
     public static void input() {
-
+        //输入数组元素
         for (int r = 0; r < arows; r++) {
             for (int c = 0; c < acols; c++) {
                 a[r][c] = reader.nextInt();
@@ -32,11 +34,14 @@ class TestMatrix {
 }
 
 class IntergerMatrix {
+    //获取行数、列数
     public static int arows = TestMatrix.reader.nextInt();
     public static int acols = TestMatrix.reader.nextInt();
+    //声明结果数组
     public static int[][] result = new int[arows][acols];
 
     public static void add(int[][] a, int[][] b) {
+        //矩阵加法
         for (int r = 0; r < a.length; r++) {
             for (int c = 0; c < a[0].length; c++) {
                 result[r][c] = calElm();
